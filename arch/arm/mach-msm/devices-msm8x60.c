db3edaf0d882c3ded1413f3ec48ab013b99b1b50
Ryrzy <ryrzy85@gmail.com>
2012-08-23 13:54:56 +0200

    config: OTG settings update

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -2056,7 +2056,7 @@ struct platform_device msm_device_gadget_peripheral = {
 		.coherent_dma_mask	= 0xffffffffULL,
 	},
 };
-#ifdef CONFIG_USB_EHCI_MSM_72K
+
 
 static struct resource resources_hsusb_host[] = {
 	{
@@ -2082,7 +2082,7 @@ struct platform_device msm_device_hsusb_host = {
 	},
 };
 
-
+#ifdef CONFIG_USB_EHCI_MSM_72K
 static struct platform_device *msm_host_devices[] = {
 	&msm_device_hsusb_host,
 };
